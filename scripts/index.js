f5a316846e4290b67f11a94811f3364541ce929a
srysis <jashandenis@gmail.com>
2024-06-19 17:52:53 +0200

    added script to add functionality for toggling sidebar

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let aside = document.querySelector("aside");
+let aside_content = document.querySelector("#aside_content");
+let sidebar_activation_button = document.querySelector("button#sidebar_menu");
+
+function toggleAside() {
+	aside.classList.toggle("hidden");
+	aside.classList.toggle("visible");
+
+	aside_content.classList.toggle("hidden");
+	aside_content.classList.toggle("visible");
+}
+
+sidebar_activation_button.addEventListener('click', function() {
+	toggleAside();
+});
+
+
+aside.addEventListener('click', function() {
+	if ((event.target.tagName == 'DIV') && (event.target.className == 'overlay')) toggleAside();
+});
